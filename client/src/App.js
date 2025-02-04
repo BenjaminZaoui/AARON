bb7ee1ed6733898d73f955943cab39c22a4fa863
BenjaminZaoui <benjaminzaouipro@gmail.com>
2025-02-04 10:05:34 +0100

    NICU

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and BIJOUR.
+          Edit <code>src/App.js</code> and BIJOUR V2.
         </p>
         <a
           className="App-link"
