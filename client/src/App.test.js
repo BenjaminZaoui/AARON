98406070a774295018fd793f91ed63275a7af8e4
BenjaminZaoui <benjaminzaouipro@gmail.com>
2025-03-16 10:23:29 +0100

    NICU FINAL

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -5,13 +5,13 @@ function sum(a,b)
   return a + b;
 }
 
-/*
+
 test('addition correcte', () => {
   expect(sum(2, 3)).toEqual(5); // Ce test réussira
 });
-*/
 
+/*
 test('addition incorrecte', () => {
   expect(sum(2, 3)).toBe(6); // Ce test échouera
 });
-
+*/
